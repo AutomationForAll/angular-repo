58b4c5d638a4ce5928497d33bb29308f3b4b985d
AutomationForAll <159029604+AutomationForAll@users.noreply.github.com>
2024-06-11 05:36:34 +0000

    styling added

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {TopheaderComponent} from '../topheader/topheader.component'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TopheaderComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
