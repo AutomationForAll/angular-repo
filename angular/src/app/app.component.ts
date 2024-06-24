1af4d7aedc09183d2935ec7f0e386ccc0a2d9d6f
AutomationForAll <159029604+AutomationForAll@users.noreply.github.com>
2024-06-24 06:17:33 +0000

    angular life cycle hook

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -7,12 +7,13 @@ import { ComponentrefComponent } from './componentref/componentref.component';
 import { NgcontentComponent } from './ngcontent/ngcontent.component';
 import { SiblingComponent } from './sibling/sibling.component';
 import { NgphasesComponent } from './ngphases/ngphases.component';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ComponentrefComponent,TempleteRefeComponent,NgcontentComponent,SiblingComponent,NgphasesComponent],
+  imports: [RouterOutlet,ComponentrefComponent,TempleteRefeComponent,NgcontentComponent,SiblingComponent,NgphasesComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,6 +22,7 @@ export class AppComponent {
   appmsg:string
   appText:string=""
   componnetDataViaApp:string=''
+  toDestroy:boolean=false;
 
   parentAppMethod(value:string){
     console.log(value)
@@ -30,8 +32,23 @@ export class AppComponent {
   @ViewChild('ComponentrefComponent')componentdata:ElementRef;
 
   onChange1(inputdata:HTMLInputElement){
-    console.log(inputdata.value)
+   // console.log(inputdata.value)
    this.appmsg=inputdata.value
+  }
+
+  constructor(){
+    console.log("patent cons")
+  }
+
+  ngAfterViewInit(): void {
+    console.log('parent ngAfterViewInit hook called')
+    //console.log(this.childdata)
+  }
+
+  destroy(){
+    this.toDestroy=!this.toDestroy
+
+
   }
 
 }
--- angular/src/app/ngphases/ngphases.component.ts
+++ angular/src/app/ngphases/ngphases.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-ngphases',
@@ -7,13 +7,56 @@ import { Component, Input } from '@angular/core';
   templateUrl: './ngphases.component.html',
   styleUrl: './ngphases.component.css'
 })
-export class NgphasesComponent {
+export class NgphasesComponent implements OnChanges,OnInit,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy{
 
-  @Input()msg:string
+  @Input()msg:string;
+
+@ViewChild('child1') childdata: ElementRef;
+
+@ContentChild('para')paracon:ElementRef
 
   constructor()
   {
+    console.log("child cons")
     console.log(this.msg)
   }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy hook called')
+  }
+  ngAfterViewChecked(): void {
+   console.log('ngAfterViewChecked hook called')
+   console.log(this.childdata)
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit hook called')
+    console.log(this.childdata)
+  }
+  ngAfterContentChecked(): void {
+   console.log('ngAfterContentChecked hook called')
+   console.log(this.paracon)
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit hook call')
+    console.log(this.paracon)
+  }
+  ngDoCheck(): void {
+    console.log("docheck hook call")
+    console.log(this.msg)
+    console.log('do check ',this.paracon)
+  }
+  ngOnInit(): void {
+    
+    console.log('on init hook call')
+    console.log(this.msg)
+    //console.log(this.childdata.nativeElement.value)
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('on ngOnChanges hook call');
+    console.log(this.msg)
+    //console.log(changes['msg'].currentValue)
+    //console.log(changes['msg'].previousValue)
+  }
+
+
 
 }
